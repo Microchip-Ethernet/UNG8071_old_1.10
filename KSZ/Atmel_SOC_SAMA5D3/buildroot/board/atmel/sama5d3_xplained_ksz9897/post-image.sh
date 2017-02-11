48fa3b0b9aadf25579d6680f935915d69a629593
Tristram Ha <Tristram.Ha@micrel.com>
2017-02-10 17:54:39 -0800

    Add Device Tree configuration for UNG8087 board.

--- KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
+++ KSZ/Atmel_SOC_SAMA5D3/buildroot/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-cp -p ${BINARIES_DIR}/at91-sama5d3_xplained_ksz9897.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME .config | cut -d"\"" -f2)
+
+cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
 
